34a708e24e3417a1b1c4ab9d72723a8fee8df356
Yuan <zhaoyuan.it@gmail.com>
2020-10-30 22:33:28 +1100

    Initial_update
    First commit to github

---
+++ criteria/id_loss.py
@@ -0,0 +1,51 @@
+import torch
+from torch import nn
+from configs.paths_config import model_paths
+from models.encoders.model_irse import Backbone
+
+
+class IDLoss(nn.Module):
+    def __init__(self):
+        super(IDLoss, self).__init__()
+        print('Loading ResNet ArcFace')
+        self.facenet = Backbone(input_size=112, num_layers=50, drop_ratio=0.6, mode='ir_se')
+        self.facenet.load_state_dict(torch.load(model_paths['ir_se50']))
+        self.face_pool = torch.nn.AdaptiveAvgPool2d((112, 112))
+        self.facenet.eval()
+
+    def extract_feats(self, x):
+        x = x[:, :, 35:223, 32:220]  # Crop interesting region
+        x = self.face_pool(x)
+        x_feats = self.facenet(x)
+        return x_feats
+
+    def forward(self, y_hat, y, x):
+        n_samples = x.shape[0]
+        x_feats = self.extract_feats(x)
+        y_feats = self.extract_feats(y)  # Otherwise use the feature from there
+        y_hat_feats = self.extract_feats(y_hat)
+        """
+        detach()
+        返回一个新的Variable，从当前计算图中分离下来的，但是仍指向原变量的存放位置,不同之处只是requires_grad为false，
+        得到的这个Variable永远不需要计算其梯度，不具有grad。即使之后重新将它的requires_grad置为true,它也不会具有梯度grad
+        这样我们就会继续使用这个新的Variable进行计算，
+        后面当我们进行反向传播时，到该调用detach()的Variable就会停止，不能再继续向前进行传播
+        """
+        y_feats = y_feats.detach()
+        loss = 0
+        sim_improvement = 0
+        id_logs = []
+        count = 0
+        for i in range(n_samples):
+            diff_target = y_hat_feats[i].dot(y_feats[i])
+            diff_input = y_hat_feats[i].dot(x_feats[i])
+            diff_views = y_feats[i].dot(x_feats[i])
+            id_logs.append({'diff_target': float(diff_target),
+                            'diff_input': float(diff_input),
+                            'diff_views': float(diff_views)})
+            loss += 1 - diff_target
+            id_diff = float(diff_target) - float(diff_views)
+            sim_improvement += id_diff
+            count += 1
+
+        return loss / count, sim_improvement / count, id_logs
---
+++ scripts/train.py
@@ -0,0 +1,50 @@
+"""
+This file runs the main training/val loop
+"""
+
+"""
+python scripts/train.py \
+--dataset_type=ffhq_encode \
+--exp_dir=/path/to/experiment \
+--workers=8 \
+--batch_size=8 \
+--test_batch_size=8 \
+--test_workers=8 \
+--val_interval=2500 \
+--save_interval=5000 \
+--encoder_type=GradualStyleEncoder \
+--start_from_latent_avg \
+--lpips_lambda=0.8 \
+--l2_lambda=1 \
+--id_lambda=0.1
+"""
+
+import os
+import json
+import sys
+import pprint
+
+sys.path.append(".")
+sys.path.append("..")
+
+from options.train_options import TrainOptions
+from training.coach import Coach
+
+
+def main():
+	opts = TrainOptions().parse()
+	if os.path.exists(opts.exp_dir):
+		raise Exception('Oops... {} already exists'.format(opts.exp_dir))
+	os.makedirs(opts.exp_dir)
+
+	opts_dict = vars(opts)
+	pprint.pprint(opts_dict)
+	with open(os.path.join(opts.exp_dir, 'opt.json'), 'w') as f:
+		json.dump(opts_dict, f, indent=4, sort_keys=True)
+
+	coach = Coach(opts)
+	coach.train()
+
+
+if __name__ == '__main__':
+	main()
